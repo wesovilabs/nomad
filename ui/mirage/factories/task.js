a0b9d0ea1a6b55ff0b11f180cf2586af2373598f
Michael Lange <dingoeatingfuzz@gmail.com>
2020-09-01 09:49:36 -0700

    Revert "Temporarily remove poststop from the UI"
    This reverts commit f8a505ea2f9def2a7ce7a93e6d0529e1b5da4894.

--- ui/mirage/factories/task.js
+++ ui/mirage/factories/task.js
@@ -19,7 +19,7 @@ export default Factory.extend({
   Resources: generateResources,
 
   Lifecycle: i => {
-    const cycle = i % 5;
+    const cycle = i % 6;
 
     if (cycle === 0) {
       return null;
@@ -31,6 +31,8 @@ export default Factory.extend({
       return { Hook: 'poststart', Sidecar: false };
     } else if (cycle === 4) {
       return { Hook: 'poststart', Sidecar: true };
+    } else if (cycle === 5) {
+      return { Hook: 'poststop' };
     }
   },
 });
