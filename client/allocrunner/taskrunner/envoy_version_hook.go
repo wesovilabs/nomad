2943c6ddd8a858e6a7d066a050de2299486892cb
Seth Hoenig <shoenig@hashicorp.com>
2020-09-09 11:53:50 -0500

    wip cleanup image thing

--- client/allocrunner/taskrunner/envoy_version_hook.go
+++ client/allocrunner/taskrunner/envoy_version_hook.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	hclog "github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-version"
 	ifs "github.com/hashicorp/nomad/client/allocrunner/interfaces"
 	"github.com/hashicorp/nomad/client/consul"
 	"github.com/hashicorp/nomad/nomad/structs"
@@ -20,6 +21,10 @@ const (
 	//
 	// This is the version defaulted by Nomad before v1.0.
 	envoyLegacyImage = "envoyproxy/envoy:v1.11.2@sha256:a7769160c9c1a55bb8d07a3b71ce5d64f72b1f665f10d81aa1581bc3cf850d09"
+
+	// envoyImageFormat is the format string used for official envoy Docker images
+	// with the tag being the semver of the version of envoy.
+	envoyImageFormat = "envoyproxy/envoy:%s"
 )
 
 type envoyVersionHookConfig struct {
@@ -74,21 +79,24 @@ func (h *envoyVersionHook) Prestart(ctx context.Context, request *ifs.TaskPresta
 
 	fmt.Println("skip is false")
 
-	// it's either legcay or managable, need to know consul version
+	// it's either legacy or manageable, need to know consul version
 	proxies, err := h.proxiesClient.Proxies()
 	if err != nil {
 		return err
 	}
 
-	if proxies == nil {
-		fmt.Println("proxies result is nil, use fallback")
-	} else {
-		fmt.Println("proxies:", proxies)
+	image, err := h.image(proxies)
+	if err != nil {
+		return err
 	}
 
-	// todo obvious
+	fmt.Println("using envoy image:", image)
+	// patch the alloc?
+	// can i just do this?
+	request.Task.Config["image"] = image
+
 	response.Done = true
-	return errors.New("not yet finished")
+	return nil
 }
 
 // skip will return true if the request does not contain a task that should have
@@ -116,4 +124,28 @@ func (_ *envoyVersionHook) isSentinel(config map[string]interface{}) bool {
 	}
 
 	return image == structs.ConnectEnvoySentinel
+}
+
+func (_ *envoyVersionHook) image(supported map[string][]string) (string, error) {
+	versions := supported["envoy"]
+	if len(versions) == 0 {
+		return envoyLegacyImage, nil
+	}
+
+	latest, err := semver(versions[0])
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf(envoyImageFormat, latest), nil
+}
+
+// semver sanitizes the envoy version string coming from Consul into the format
+// used by the Envoy project when publishing images (i.e. proper semver).
+func semver(chosen string) (string, error) {
+	v, err := version.NewVersion(chosen)
+	if err != nil {
+		return "", errors.Wrap(err, "unexpected envoy version format")
+	}
+	return "v" + v.String(), nil
 }
